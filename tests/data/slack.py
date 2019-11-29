05cd5b74b9e1043e2db681dc3113abab02b3285f
Matt Warren <matt@warrencomputing.net>
2019-11-29 08:07:48 -0500

    test data excluded via config

--- tests/data/slack.py
+++ tests/data/slack.py
@@ -1,4 +1,3 @@
-# noqa
 add_tip_form_post = 'token=wglrkjeelkjefel&team_id=T0000JGRL&team_domain=test-data&channel_id=DQQQQPUBA&channel_name=directmessage&user_id=U1337LGTTN&user_name=matt&command=%2Fhattip&text=%40mattwwarren+blah+blah+blah+isn%27t+that+so+extra+super+wonderful%3F&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT0000JGRL%2F845954834913%2F9876543456789876543&trigger_id=842417487123.241952628870.fe9c781b4677a399220a155e95cc8357'
 add_tip_json = {
         'token': 'wglrkjeelkjefel',
