6cd8063a99609779e7daa7fc52e07ccb43aa85a4
Matt Warren <matt@warrencomputing.net>
2019-11-29 07:16:53 -0500

    handle Heroku dynamic PORT var

--- hattip/async_app.py
+++ hattip/async_app.py
@@ -108,7 +108,7 @@ def main():
     app = build_webapp()
     app.on_startup.append(start_background_tasks)
     app.on_cleanup.append(cleanup_background_tasks)
-    web.run_app(app)
+    web.run_app(app, port=os.environ.get('PORT', 8080))
 
 
 if __name__ == "__main__":
